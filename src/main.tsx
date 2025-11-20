04f7443b6fc502a11dbeda178220a5d3fc21248a
dennisrabinovitch <rdennis0993@gmail.com>
2025-11-20 14:11:52 +0200

    feat: add scroll to top on route change

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Router from './router.tsx';
+import ScrollToTop from './components/ScrollToTop';
 import './i18n';
 import './styles/main.scss';
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter basename="/Motiv8-React">
+        <ScrollToTop />
         <Router />
       </BrowserRouter>
     </HelmetProvider>
